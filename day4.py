50d4a0c20b4a08ca5b228b0c12bd15ee4a21f82e
Rob vd Bogert <robvdbogert@gmail.com>
2016-12-09 17:27:10 +0100

    day 4, part 2

--- day4.py
+++ day4.py
@@ -3,6 +3,8 @@ from collections import Counter
 
 
 class RoomValidator:
+    _shiftmap = None
+
     def validate(self, room: str):
         room_regex = re.compile(r'^([a-z-]+)(\d+)\[([a-z]{5})\]$')
 
@@ -13,7 +15,8 @@ class RoomValidator:
             sector_id = int(groups[1])
             checksum = groups[2]
             generated_checksum = self.get_checksum(encrypted_name)
-            return checksum == generated_checksum, sector_id
+            decrypted_name = self.decrypt_name(encrypted_name, sector_id)
+            return checksum == generated_checksum, sector_id, decrypted_name
 
         return False, None
 
@@ -23,6 +26,19 @@ class RoomValidator:
         letter_counts.sort(key=lambda x: x[1], reverse=True)
         return ''.join(map(lambda x: x[0], letter_counts))[:5]
 
+    def decrypt_name(self, encrypted_name: str, sector_id: int):
+        shift = sector_id % 26
+        result = ''
+        for letter in encrypted_name:
+            if letter == '-':
+                result += ' '
+            else:
+                if ord(letter) + shift > ord('z'):
+                    result += chr(ord(letter) - (26 - shift))
+                else:
+                    result += chr(ord(letter) + shift)
+        return result.rstrip()
+
 
 if __name__ == '__main__':
     with open('./input/day4') as f:
@@ -31,8 +47,12 @@ if __name__ == '__main__':
     validator = RoomValidator()
     sum_sector_ids = 0
     for line in data:
-        (valid, sector_id) = validator.validate(line)
+        (valid, sector_id, name) = validator.validate(line)
         if valid:
             sum_sector_ids += sector_id
+        if 'north' in name and 'pole' in name:
+            print("North Pole objects stored in '{}', sector id '{}'".format(
+                name, sector_id
+            ))
 
     print('Sum of sector ids = ' + str(sum_sector_ids))
--- test/test_day4.py
+++ test/test_day4.py
@@ -7,17 +7,20 @@ class TestRoomValidator(unittest.TestCase):
     def test_validate(self):
         validator = RoomValidator()
 
-        (valid, sector_id) = validator.validate('aaaaa-bbb-z-y-x-123[abxyz]')
+        (valid, sector_id, name) = validator.validate('aaaaa-bbb-z-y-x-123[abxyz]')
         self.assertTrue(valid)
 
-        (valid, sector_id) = validator.validate('bbbbb-aaa-z-y-x-123[baxyz]')
+        (valid, sector_id, name) = validator.validate('bbbbb-aaa-z-y-x-123[baxyz]')
         self.assertTrue(valid)
 
-        (valid, sector_id) = validator.validate('a-b-c-d-e-f-g-h-987[abcde]')
+        (valid, sector_id, name) = validator.validate('a-b-c-d-e-f-g-h-987[abcde]')
         self.assertTrue(valid)
 
-        (valid, sector_id) = validator.validate('not-a-real-room-404[oarel]')
+        (valid, sector_id, name) = validator.validate('not-a-real-room-404[oarel]')
         self.assertTrue(valid)
 
-        (valid, sector_id) = validator.validate('totally-real-room-200[decoy]')
+        (valid, sector_id, name) = validator.validate('totally-real-room-200[decoy]')
         self.assertFalse(valid)
+
+        (valid, sector_id, name) = validator.validate('qzmt-zixmtkozy-ivhz-343[blaat]')
+        self.assertEqual('very encrypted name', name)
