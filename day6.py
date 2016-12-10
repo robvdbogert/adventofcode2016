ef48cc48cae2ea503430261c7e158edb81c3dad3
Rob vd Bogert <robvdbogert@gmail.com>
2016-12-10 16:16:40 +0100

    day 6, part 2

--- day6.py
+++ day6.py
@@ -1,5 +1,5 @@
 class MessageRecovery:
-    def get_message(self, data):
+    def get_message(self, data, use_most_common_letter=True):
         letter_counts = [{} for x in range(0, len(data[0]))]
 
         for line in data:
@@ -8,7 +8,7 @@ class MessageRecovery:
                     letter_counts[index][char] = 0
                 letter_counts[index][char] += 1
 
-        result = ''.join([sorted(x, key=lambda k: x[k], reverse=True)[0] for x in letter_counts])
+        result = ''.join([sorted(x, key=lambda k: x[k], reverse=use_most_common_letter)[0] for x in letter_counts])
         return result
 
 
@@ -16,5 +16,9 @@ if __name__ == '__main__':
     with open('./input/day6') as f:
         data = f.read().split()
 
-    message = MessageRecovery().get_message(data)
-    print('Message: ' + message)
+    recovery = MessageRecovery()
+    message = recovery.get_message(data)
+    print('Message using most common letter in each column: ' + message)
+
+    message = recovery.get_message(data, False)
+    print('Message using least common letter in each column: ' + message)
--- test/test_day6.py
+++ test/test_day6.py
@@ -4,25 +4,29 @@ from day6 import MessageRecovery
 
 
 class TestMessageRecovery(unittest.TestCase):
-    def test_recover(self):
-        data = [
-            'eedadn',
-            'drvtee',
-            'eandsr',
-            'raavrd',
-            'atevrs',
-            'tsrnev',
-            'sdttsa',
-            'rasrtv',
-            'nssdts',
-            'ntnada',
-            'svetve',
-            'tesnvt',
-            'vntsnd',
-            'vrdear',
-            'dvrsen',
-            'enarar'
-        ]
+    data = [
+        'eedadn',
+        'drvtee',
+        'eandsr',
+        'raavrd',
+        'atevrs',
+        'tsrnev',
+        'sdttsa',
+        'rasrtv',
+        'nssdts',
+        'ntnada',
+        'svetve',
+        'tesnvt',
+        'vntsnd',
+        'vrdear',
+        'dvrsen',
+        'enarar'
+    ]
 
-        message = MessageRecovery().get_message(data)
+    def test_recover_using_most_common_letter(self):
+        message = MessageRecovery().get_message(self.data)
         self.assertEqual('easter', message)
+
+    def test_recover_using_least_common_letter(self):
+        message = MessageRecovery().get_message(self.data, False)
+        self.assertEqual('advent', message)
