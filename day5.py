0c604058551fa40a81e04a387ed46af4d6d852a3
Rob vd Bogert <robvdbogert@gmail.com>
2016-12-10 14:33:44 +0100

    day 5, part 2

--- day5.py
+++ day5.py
@@ -15,7 +15,29 @@ class DoorPasswordCracker:
 
         return result
 
+    def hack_part_two(self, door_id: str):
+        index = 0
+        chars_found = 0
+
+        result = list('________')
+        md5_hasher = hashlib.md5
+        while chars_found < 8:
+            md5 = md5_hasher((door_id + str(index)).encode('utf-8')).hexdigest()
+            if md5[:5] == '00000':
+                if str.isdigit(md5[5]):
+                    pos = int(md5[5])
+                else:
+                    pos = 100
+                if pos < 8 and result[pos] == '_':
+                    result[pos] = md5[6]
+                    chars_found += 1
+            index += 1
+
+        return ''.join(result)
 
 if __name__ == '__main__':
     password = DoorPasswordCracker().hack('ugkcyxxp')
     print('Password = ' + password)
+
+    password_door_2 = DoorPasswordCracker().hack_part_two('ugkcyxxp')
+    print('Password door 2 = ' + password_door_2)
--- test/test_day5.py
+++ test/test_day5.py
@@ -4,7 +4,12 @@ from day5 import DoorPasswordCracker
 
 
 class TestDoorPasswordCracker(unittest.TestCase):
-    def test_crack(self):
+    def test_hack(self):
         cracker = DoorPasswordCracker()
         password = cracker.hack('abc')
         self.assertEqual('18f47a30', password)
+
+    def test_hack_part_two(self):
+        cracker = DoorPasswordCracker()
+        password = cracker.hack_part_two('abc')
+        self.assertEqual('05ace8e3', password)
